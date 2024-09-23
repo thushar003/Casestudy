cfe272ef3b6e229d51287a152e54f3c7ca96efbc
thushar003 <thusharjoji2@gmail.com>
2024-09-23 15:06:19 -0400

    Backup query for creating the Helpdesk database saved, tests for add, update and delete completed

--- CasestudyTests/DAOTests.cs
+++ CasestudyTests/DAOTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,7 @@ namespace CasestudyTests
         public async Task Employee_GetByPhoneNumberTest()
         {
             EmployeeDAO dao = new();
-            Employee selectedEmployee = await dao.GetByPhoneNumber("555-555-5555");
+            Employee selectedEmployee = await dao.GetByPhoneNumber("(555) 555-5555");
             Assert.NotNull(selectedEmployee);
         }
 
@@ -44,7 +45,40 @@ namespace CasestudyTests
         [Fact]
         public async Task Employee_AddTest()
         {
-            //TO-DO
+            EmployeeDAO dao = new();
+            Employee newEmployee = new()
+            {
+                Title = "Mr.",
+                FirstName = "Thushar",
+                LastName = "Joji",
+                PhoneNo = "(226)998-9806",
+                Email = "tj@abc.com",
+                DepartmentId = 100,
+            };
+            Assert.True(await dao.Add(newEmployee) > 0);
+        }
+
+        [Fact]
+        public async Task Employee_UpdateTest()
+        {
+            EmployeeDAO dao = new();
+            Employee? employeeForUpdate = await dao.GetByEmail("tj@abc.com");
+
+            if (employeeForUpdate != null)
+            {
+                string oldPhoneNo = employeeForUpdate.PhoneNo;
+                string newPhoneNo = oldPhoneNo == "226-998-9806" ? "555-555-5555" : "226-998-9806";
+                employeeForUpdate!.PhoneNo = newPhoneNo;
+            }
+            Assert.True(await dao.Update(employeeForUpdate!) == 1);     //1 indicates number of rows updated
+        }
+
+        [Fact]
+        public async Task Employee_DeleteTest()
+        {
+            EmployeeDAO dao = new();
+            Employee? employeeToDelete = await dao.GetById(1);
+            Assert.True(await dao.Delete(employeeToDelete.Id!) == 1);
         }
     }
 }
