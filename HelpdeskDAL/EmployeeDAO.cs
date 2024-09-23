a8897f29cb92a8abd226d6de0e98691ccf52d690
thushar003 <thusharjoji2@gmail.com>
2024-09-23 14:10:32 -0400

    Added get by phone number and get all methods

--- HelpdeskDAL/EmployeeDAO.cs
+++ HelpdeskDAL/EmployeeDAO.cs
@@ -22,5 +22,39 @@ namespace HelpdeskDAL
 
             return selectedEmployee;
         }
+
+        public async Task<Employee> GetByPhoneNumber(string phoneNum)
+        {
+            Employee? selectedEmployee;
+            try
+            {
+                HelpdeskContext _db = new();
+                selectedEmployee = await _db.Employees.FirstOrDefaultAsync(emp => emp.PhoneNo == phoneNum);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+
+            return selectedEmployee;
+        }
+
+        public async Task<List<Employee>> GetAll()
+        {
+            List<Employee> allEmployees;
+            try
+            {
+                HelpdeskContext _db = new();
+                allEmployees = await _db.Employees.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+
+            return allEmployees;
+        }
     }
 }
