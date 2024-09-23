5411db0921f6a457137b30c38c5f62c7f4a62203
thushar003 <thusharjoji2@gmail.com>
2024-09-23 14:24:27 -0400

    Added add, update and delete methods

--- HelpdeskDAL/EmployeeDAO.cs
+++ HelpdeskDAL/EmployeeDAO.cs
@@ -56,5 +56,61 @@ namespace HelpdeskDAL
 
             return allEmployees;
         }
+
+        public async Task<int> Add(Employee newEmployee)
+        {
+            try
+            {
+                HelpdeskContext _db = new();
+                await _db.Employees.AddAsync(newEmployee);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+
+            return newEmployee.Id;
+        }
+
+        public async Task<int> Update(Employee updatedEmployee)
+        {
+            int employeeUpdated = -1;
+            try
+            {
+                HelpdeskContext _db = new();
+                Employee? currentEmployee =
+                    await _db.Employees.FirstOrDefaultAsync(emp => emp.Id == updatedEmployee.Id);
+                _db.Entry(currentEmployee!).CurrentValues.SetValues(updatedEmployee);
+                employeeUpdated = await _db.SaveChangesAsync();     //should return a value of 1
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+
+            return employeeUpdated;
+        }
+        public async Task<int> Delete(int? id)
+        {
+            int employeesDeleted = -1;
+            try
+            {
+                HelpdeskContext _db = new();
+                Employee? selectedEmployee =
+                    await _db.Employees.FirstOrDefaultAsync(emp => emp.Id == id);
+                _db.Employees.Remove(selectedEmployee);
+                employeesDeleted = await _db.SaveChangesAsync();        //returns number of rows removed
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+
+            return employeesDeleted;
+        }
     }
 }
