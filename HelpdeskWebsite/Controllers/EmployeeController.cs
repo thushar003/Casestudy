d754b1b6ea984dbac30aadce6093c4cfff4d19af
thushar003 <thusharjoji2@gmail.com>
2024-09-27 12:12:56 -0400

    EmployeeController added, .NET API added

---
+++ HelpdeskWebsite/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+using HelpdeskViewModels;
+
+namespace HelpdeskWebsite.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class EmployeeController : ControllerBase
+        {
+            [HttpGet("{lastname}")]
+            public async Task<IActionResult> GetByEmail(string email)
+            {
+                try
+                {
+                    EmployeeViewModel viewmodel = new() { Email = email};
+                    await viewmodel.GetByEmail();
+                    return Ok(viewmodel);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Problem in " + GetType().Name + " " +
+                                    MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                    return StatusCode(StatusCodes.Status500InternalServerError); // something went wrong
+                }
+            }
+        }
+}
