6683d634ecfe0b37b4509110d91fc99f8e1bb7bb
thushar003 <thusharjoji2@gmail.com>
2024-10-27 13:12:42 -0400

    DepartmentDAO created

---
+++ HelpdeskDAL/DepartmentDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpdeskDAL
+{
+    public class DepartmentDAO
+    {
+        readonly IRepository<Department> _repo;
+        public DepartmentDAO()
+        {
+            _repo = new HelpdeskRepository<Department>();
+        }
+
+        public async Task<List<Department>> GetAll()
+        {
+            List<Department> allDepartments;
+            try
+            {
+                allDepartments = await _repo.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " + MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                throw;
+            }
+            return allDepartments;
+        }
+    }
+}
